973d03336e6a10a26d624e2f1c34ed4a242f0e3b
Daniel Avelino <daniel.avelinoneto@gmail.com>
2026-06-08 18:37:38 -0300

    feat: cria modelo de endereço

--- src/br/com/daniel/localizacep/app/Main.java
+++ src/br/com/daniel/localizacep/app/Main.java
@@ -1,4 +1,9 @@
 package br.com.daniel.localizacep.app;
 
+import br.com.daniel.localizacep.service.ConsumoApi;
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/br/com/daniel/localizacep/model/Endereco.java
@@ -0,0 +1,4 @@
+package br.com.daniel.localizacep.model;
+
+public record Endereco(String cep, String logradouro, String complemento,String localidade, String uf) {
+}
