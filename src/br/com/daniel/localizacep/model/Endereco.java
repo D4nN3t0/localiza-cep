b35676b79540e10896f99837dfaedad4fc3fce6d
Daniel Avelino <daniel.avelinoneto@gmail.com>
2026-06-08 19:58:38 -0300

    refactor: melhora exibição do endereço formatado

--- src/br/com/daniel/localizacep/model/Endereco.java
+++ src/br/com/daniel/localizacep/model/Endereco.java
@@ -1,5 +1,29 @@
 package br.com.daniel.localizacep.model;
 
-public record Endereco(String cep, String logradouro, String complemento,String localidade, String uf) {
-
+public record Endereco(String cep, String logradouro, String complemento, String localidade, String uf) {
+    @Override
+    public String toString() {
+        String complementoFormatado =
+                complemento.isBlank()
+                        ? "Não informado"
+                        : complemento;
+        return """
+                
+                ==============================
+                ENDEREÇO ENCONTRADO
+                ==============================
+                CEP: %s
+                LOGRADOURO: %s
+                COMPLEMENTO: %s
+                CIDADE: %s
+                UF: %s
+                ==============================
+                """.formatted(
+                cep,
+                logradouro,
+                complementoFormatado,
+                localidade,
+                uf
+        );
+    }
 }
